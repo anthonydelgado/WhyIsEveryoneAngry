cfaeb0e83cb31e5f2ed1f4008a5f9fc686204990
David H <davidhamberlin@gmail.com>
2017-06-25 03:14:03 -0400

    add bar chart showing avg emotes / frame

--- charts/charts.js
+++ charts/charts.js
@@ -1,36 +1,47 @@
-const chartLabels = ['Seconds',
-  'anger',
-  'contempt',
-  'disgust',
-  'fear',
-  'happiness',
-  'neutral',
-  'sadness',
-  'surprise' ]
-const chartOptions = {
+const lineLabels = ['Seconds',
+  'Anger',
+  'Contempt',
+  'Disgust',
+  'Fear',
+  'Happiness',
+  // 'Neutral',
+  'Sadness',
+  'Surprise' ]
+const lineOptions = {
   title: 'Audience Reaction',
   curveType: 'function',
-  legend: { position: 'right' }
+  legend: { position: 'right' },
+  chartArea: {width: '50%'},
+  chartAlign: 'left'
 };
-let lineChart;
+const barLabels = lineLabels.slice(1);
+const barOptions =  {
+  title: 'BARS, YO!',
+  chartArea: {width: '50%'}
+}
+
+let lineChart, barChart;
 
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(createChart);
+google.charts.setOnLoadCallback(createCharts);
 
-function createChart() {
+function createCharts() {
   lineChart = new google.visualization.LineChart(document.getElementById('time-series'));
+  barChart = new google.visualization.BarChart(document.getElementById('bar-chart'));
 }
 
-function drawChart() {
-  const data = google.visualization.arrayToDataTable(format(imageData));
-  lineChart.draw(data, chartOptions);
+function drawCharts() {
+  const lineFormatted = lineFormat(imageData);
+  const barFormatted = barFormat(lineFormatted);
+  const lineData = google.visualization.arrayToDataTable(lineFormatted);
+  const barData = google.visualization.arrayToDataTable(barFormatted);
+  lineChart.draw(lineData, lineOptions);
+  barChart.draw(barData, barOptions);
 }
 
-function format(emoData) {
-  const data = [chartLabels];
-  console.log('INPUT: ', emoData);
-  _.each(emoData, pic => {
-    console.log('pic: ', pic)
+function lineFormat(imgData) {
+  const data = [lineLabels];
+  _.each(imgData, pic => {
     const faceScores = pic.emotions.map(face => face.scores);
     if (faceScores.length > 0) {
       const emotesTotal = faceScores.reduce((acc, face) => ({
@@ -39,28 +50,27 @@ function format(emoData) {
         disgust: face.disgust + acc.disgust,
         fear: face.fear + acc.fear,
         happiness: face.happiness + acc.happiness,
-        neutral: face.neutral + acc.neutral,
+        // neutral: face.neutral + acc.neutral,
         sadness: face.sadness + acc.sadness,
         surprise: face.surprise + acc.surprise,
       }));
+      emotesTotal.neutral !== undefined && delete emotesTotal.neutral;
       const emotesAvg = _.map(emotesTotal, emote => emote / pic.emotions.length);
-      let time = msToTime(pic.time)
+      let time = millisToMinutesAndSeconds(pic.time)
       const result = [time, ...emotesAvg];
       data.push(result);
     }
   })
-  console.log('OUTPUT: ', data)
   return data;
 }
 
-
-function msToTime(timeStamp) {
-  const ms = timeStamp % 1000;
-  timeStamp = (timeStamp - ms) / 1000;
-  const secs = timeStamp % 60;
-  timeStamp = (timeStamp - secs) / 60;
-  const mins = timeStamp % 60;
-  const hrs = (timeStamp - mins) / 60;
-
-  return (hrs ? hrs + ':' : '') + (mins ? mins + ':' : '') + secs;
+function barFormat(lineData) {
+  const frameCount = lineData.length - 1;
+  const totals = lineData.slice(1)
+                          .map(frame => frame.slice(1))
+                          .reduce((acc, frame) => frame.map((emote, i) => emote + acc[i]))
+  const averages = totals.map(emote => emote / frameCount);
+  const formatted = averages.map((avg, i) => [barLabels[i], avg]);
+  const sorted = formatted.sort((a, b) => b[1] - a[1])
+  return [['y', 'x'], ...sorted];
 }
